70538ea3200bec539158eebaf642fcdf3b219d67
kalai19 <35715506+kalai19@users.noreply.github.com>
2018-05-19 20:17:42 +0530

    Add files via upload

---
+++ fibonacci.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+ 
+int main()
+{
+	int a=0,b=1,n,c;
+	printf("\n enter the number");
+	scanf("%d",&n);
+	printf("\n fibonacci series are");
+	printf("\n%d",a);
+	printf("\n%d",b);
+	while(n>2)
+	{
+		c=a+b;
+		printf("\n%d",c);
+		a=b;
+		b=c;
+		n--;
+		}
+		
+	return 0;
+}
+ 
